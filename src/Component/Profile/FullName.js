d608a3b58f92ac7d3c3ff60391b176e2a82f81cc
ASUS <aatidelkouki@gmail.com>
2021-04-07 23:52:20 +0100

    important

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ComponentTwo = () =>{
+    return(
+    <h2>Hello! My name is Itidel Kouki</h2>
+)
+}
+export default ComponentTwo;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const ComponentOne = () =>{
+    return(
+    <div className="head-page">
+      <h3>My beautiful picture</h3>
+
+      <img
+      className="picture-100"
+      src= "./photo1.jpg"
+      alt="myImage"
+      />
+    </div>
+      )
+    }
+    export default ComponentOne
